8667c000996c0ef641c4de8ccf807e11e3e35cc8
mwlky <mwlky@getgoogleoff.me>
2022-12-14 20:56:51 +0100

    changed picking up pill

--- Assets/Code/Controllers/PlayerNavMeshController.cs
+++ Assets/Code/Controllers/PlayerNavMeshController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,14 +9,31 @@ public class PlayerNavMeshController : MonoBehaviour
     [SerializeField] private Transform _movePositionWaypoint;
 
     private NavMeshAgent _navMeshAgent;
+    private bool isPlayerHoldingPill;
 
-    private void Awake()
-    {
+    private void Awake() =>
         _navMeshAgent = GetComponent<NavMeshAgent>();
-    }
 
-    private void Update()
+    private void OnEnable() =>
+        PickUpController.onPlayerHoldingTheObject += PlayerPickedUpPill;
+    
+    private void OnDisable() =>
+        PickUpController.onPlayerHoldingTheObject -= PlayerPickedUpPill;
+    
+    private void Update() =>
+        SetDestination();
+
+    void PlayerPickedUpPill(bool holding) =>
+        isPlayerHoldingPill = holding;
+    
+    void SetDestination()
     {
+        if (isPlayerHoldingPill)
+        {
+            _navMeshAgent.destination = transform.position;
+            return;
+        }
+
         _navMeshAgent.destination = _movePositionWaypoint.position;
     }
 }
