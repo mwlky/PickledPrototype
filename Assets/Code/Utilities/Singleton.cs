733709d5dde9f69cbb4d58e33395df57ff8dc196
styxu2153 <kuba281197@gmail.com>
2022-12-15 23:18:37 +0100

    added draw path to pill;
    Path is drawing correctly but only when pill in on ground. Gonna work on this

---
+++ Assets/Code/Utilities/Singleton.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T:Component
+{
+    private static T _instance;
+
+    public static T Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<T>();
+
+                if (_instance == null)
+                {
+                    GameObject gameObject = new GameObject("Controller");
+                    _instance = gameObject.AddComponent<T>();
+                }
+            }
+            return _instance;
+        }
+    }
+
+    private void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this as T;
+        }
+        else
+        {
+            if (_instance != this)
+                Destroy(gameObject);
+        }
+    }
+}
