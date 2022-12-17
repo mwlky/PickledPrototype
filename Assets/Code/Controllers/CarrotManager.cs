131e67a8ba1eb6ec763e31cc73498d13264483d7
styxu2153 <kuba281197@gmail.com>
2022-12-17 17:27:06 +0100

    fixes to UI

--- Assets/Code/Controllers/CarrotManager.cs
+++ Assets/Code/Controllers/CarrotManager.cs
@@ -11,7 +11,7 @@ public class CarrotManager : MonoBehaviour
     public static List<GameObject> carrotsCount = new();
     private int amountOfCarrots;
 
-    private void Start() =>
+    private void Awake() =>
         CountAllCarrots();
 
     private void OnEnable() =>
